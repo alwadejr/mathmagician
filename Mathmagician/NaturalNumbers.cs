6a15c655100fb699dfd7e882200c296150a91f1c
alwadejr <lewis@mrwade.net>
2015-10-23 14:31:23 -0500

    Completed Natural Numbers Implementation with tests-- Started Even numbers

---
+++ Mathmagician/NaturalNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+   public class NaturalNumbers
+    {
+
+        private int first;
+
+        public NaturalNumbers()
+        {
+            first = 1;
+        }
+        
+        public int GetNext(int x)
+        {
+            return x + 1;
+        }
+
+        public int GetFirst()
+        {
+            return first;
+        }
+
+        public int[] GetSequence()
+        {
+            //  int[] output = new int[10];
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+            while(output.Count < 10)
+            {
+                output.Add(GetNext(output.Last()));
+            }
+
+            return output.ToArray();
+        }
+        public int[] GetSequence(int length)
+        {
+            //int[] output = new int[length];
+            
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+     
+
+            while(output.Count < length)
+            {
+                output.Add(GetNext(output.Last()));
+
+            }
+
+            return output.ToArray();
+        }
+
+        public string ToString(int[] array)
+        {
+            string output = String.Join(" ", array);
+            
+            return output;
+
+        }
+    }
+}
---
+++ MathmagicianTests/EvenTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Mathmagician;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class EvenTest
+    {
+        [TestMethod]
+        public void EnsureICanCreateClassInstance()
+        {
+            EvenNumbers even = EvenNumbers();
+            Assert.IsNotNull(even);
+
+        }
+
+
+    }
+}
